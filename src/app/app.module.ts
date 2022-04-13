091b5dfed5bd057824ad78f48a8fa8a289c80064
Gabriel Fonseca <gabrielfonsecaoficial@gmail.com>
2022-04-13 12:57:44 -0400

    feat: actions column implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 /** @description pacotes do Angular*/
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -13,6 +13,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatTableModule } from '@angular/material/table';
 import { MatInputModule } from '@angular/material/input';
 
 /**@description components do App */
@@ -26,6 +27,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { ForDirective } from './directives/for.directive';
 import { ProductCreateComponent } from './components/product/product-create/product-create.component';
 import { ProductReadComponent } from './components/product/product-read/product-read.component';
+
+import localePt from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localePt);
 
 @NgModule({
   declarations: [
@@ -52,9 +58,14 @@ import { ProductReadComponent } from './components/product/product-read/product-
     MatSnackBarModule,
     HttpClientModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatTableModule
   ],
-  providers: [],
+  providers: [{
+    provide: LOCALE_ID,
+    useValue: 'pt-BR',
+
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
